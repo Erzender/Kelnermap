f45a1b55a401fb8a1296418dfbd310061c9c23bd
Erzender <lukas.parant@epitech.eu>
2018-08-09 12:12:59 +0200

    no iframes

--- map.js
+++ map.js
@@ -1,6 +1,8 @@
 var app = new Vue({
   el: "#app",
   data: {
+    navBar: ["Map", "Empires"],
+    route: "Map",
     search: { x: 0, z: 0 },
     control: [],
     mouseMap: { left: 0, top: 0 },
@@ -15,75 +17,16 @@ var app = new Vue({
     }
   },
   methods: {
-    searchArea: function(x, z, event) {
-      if (event) event.preventDefault();
-      var supx = Math.round((x / 512 + 7) / 33 * 40);
-      var supz = Math.round((z / 512 + 11) / 33 * 40);
-      this.mouseMap = { left: Math.round(x), top: Math.round(z) };
-      this.updateArea(supx, supz);
+    setRoute: function(route) {
+      this.route = route
     },
-    toggleGrid: function() {
-      this.grid = !this.grid;
-    },
-    toggleControl: function() {
-      this.displayControl = !this.displayControl;
-    },
-    getArea: function(x, z) {
-      for (empire of this.control) {
-        for (area of empire.areas) {
-          if (area.x === x && area.z === z) {
-            return {
-              color: empire.color,
-              name: empire.name,
-              leader: empire.leader,
-              x: x,
-              z: z
-            };
-          }
-        }
-      }
-      return { color: "transparent", name: "???", leader: "aucun", x: x, z: z };
-    },
-    getColor: function(x, z) {
-      if (!this.displayControl) {
-        return "transparent";
-      }
-      if (this.area.x === x && this.area.z === z) {
-        return "white";
-      }
-      for (empire of this.control) {
-        var color = empire.color;
-        for (area of empire.areas) {
-          if (area.x === x && area.z === z) {
-            return color;
-          }
-        }
-      }
-      return "transparent";
-    },
-    updateArea: function(x, z) {
-      this.area = this.getArea(x, z);
-    },
-    updateMouseMap: function(e) {
-      var offset = {
-        left:
-          document.getElementById("map").offsetLeft -
-          document.getElementById("mapCnt").scrollLeft,
-        top:
-          document.getElementById("map").offsetTop -
-          document.getElementById("mapCnt").scrollTop,
-        width: document.getElementById("map").offsetWidth,
-        height: document.getElementById("map").offsetHeight
-      };
-      this.mouseMap = {
-        left: Math.round(
-          ((e.pageX - offset.left) / offset.width * 33 - 7) * 512
-        ),
-        top: Math.round(
-          ((e.pageY - offset.top) / offset.height * 33 - 11) * 512
-        )
-      };
-    }
+    searchArea: methodsMap.searchArea,
+    toggleGrid: methodsMap.toggleGrid,
+    toggleControl: methodsMap.toggleControl,
+    getArea: methodsMap.getArea,
+    getColor: methodsMap.getColor,
+    updateArea: methodsMap.updateArea,
+    updateMouseMap: methodsMap.updateMouseMap
   },
   mounted: function() {
     fetch("data.json").then(
---
+++ methodsMap.js
@@ -0,0 +1,71 @@
+methodsMap = {
+  searchArea: function(x, z, event) {
+    if (event) event.preventDefault();
+    var supx = Math.round((x / 512 + 7) / 33 * 40);
+    var supz = Math.round((z / 512 + 11) / 33 * 40);
+    this.mouseMap = { left: Math.round(x), top: Math.round(z) };
+    this.updateArea(supx, supz);
+  },
+  toggleGrid: function() {
+    this.grid = !this.grid;
+  },
+  toggleControl: function() {
+    this.displayControl = !this.displayControl;
+  },
+  getArea: function(x, z) {
+    for (empire of this.control) {
+      for (area of empire.areas) {
+        if (area.x === x && area.z === z) {
+          return {
+            color: empire.color,
+            name: empire.name,
+            leader: empire.leader,
+            x: x,
+            z: z
+          };
+        }
+      }
+    }
+    return { color: "transparent", name: "???", leader: "aucun", x: x, z: z };
+  },
+  getColor: function(x, z) {
+    if (!this.displayControl) {
+      return "transparent";
+    }
+    if (this.area.x === x && this.area.z === z) {
+      return "white";
+    }
+    for (empire of this.control) {
+      var color = empire.color;
+      for (area of empire.areas) {
+        if (area.x === x && area.z === z) {
+          return color;
+        }
+      }
+    }
+    return "transparent";
+  },
+  updateArea: function(x, z) {
+    this.area = this.getArea(x, z);
+  },
+  updateMouseMap: function(e) {
+    var offset = {
+      left:
+        document.getElementById("map").offsetLeft -
+        document.getElementById("mapCnt").scrollLeft,
+      top:
+        document.getElementById("map").offsetTop -
+        document.getElementById("mapCnt").scrollTop,
+      width: document.getElementById("map").offsetWidth,
+      height: document.getElementById("map").offsetHeight
+    };
+    this.mouseMap = {
+      left: Math.round(
+        ((e.pageX - offset.left) / offset.width * 33 - 7) * 512
+      ),
+      top: Math.round(
+        ((e.pageY - offset.top) / offset.height * 33 - 11) * 512
+      )
+    };
+  }
+}
