2ba6ac0a36c15e5cb70d73334a7e396c06de3843
Erzender <>
2018-08-15 17:18:00 +0200

    now it's a server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+
+creds = JSON.parse(process.env.KELNER_CRED) || {url: "localhost:8080", secret: "secret"}
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/map.html");
+});
+
+app.get("/data", function(req, res) {
+  fetch(creds.url, { method: "GET", headers: {"secret-key": creds.secret}})
+    .then(data => {
+      return data.json();
+    })
+    .then(json => {
+      res.json(json);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json([]);
+    });
+});
+
+var port = process.env.PORT || 8080;
+console.log("listening on port " + port);
+app.listen(port);
