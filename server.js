7949e4216ae233a41f9c19091e5e6d79f6d1e2cd
Erzender <>
2018-08-16 17:06:43 +0200

    glados

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    console.log(message.content);
+});
+
+client.login(process.env.KELNER_BOT);
+
 // https://jsonbin.io/
 creds = process.env.KELNER_CRED ? JSON.parse(process.env.KELNER_CRED) : {url: "https://localhost:8080", secret: "secret"}
 
