813a1893fae3f6fb1a6dc12120d6027228ac4372
Erzender <lukas.parant@epitech.eu>
2018-09-06 20:46:12 +0200

    settings feature

--- public/map.js
+++ public/map.js
@@ -2,25 +2,17 @@ var app = new Vue({
   el: "#app",
   data: {
     navBar: [
-      "Map", "Nations"
+      "Map", "Nations", "Options"
     ],
-    route: "Map",
+    route: "Options",
     navNation: "L'Empire",
     nav: {
       nationPanel: "desc",
       mapEdit: false
     },
-    descEdit: "",
-    search: {
-      x: 0,
-      z: 0
-    },
+    search: {x: 0, z: 0},
     control: [],
-    soutiens: [],
-    mouseMap: {
-      left: 0,
-      top: 0
-    },
+    mouseMap: {left: 0, top: 0},
     map: Array().fill(40),
     area: {
       color: "transparent",
@@ -31,9 +23,16 @@ var app = new Vue({
     displayControl: true,
     mapEdition: [],
     message: "",
-    descriptions: [],
-    editSoutiens: {
-      list: []
+    settings: {
+      message: "",
+      selectedPlayer: 0,
+      name: "",
+      soutiens: [],
+      toAddSoutien: 0,
+      color: "#FF00FF",
+      desc:"",
+      nationName: "",
+      image: "https://s1.qwant.com/thumbr/0x0/2/f/07b6960b07cf13295400fe43e0c2822a5effc4b4591c082a964b8505ed79e9/33505.jpg?u=http%3A%2F%2Femblemsbf.com%2Fimg%2F33505.jpg&q=0&b=1&p=0&a=1"
     }
   },
   computed: {
@@ -55,10 +54,10 @@ var app = new Vue({
         for (area of empire.areas) {
           if (colors[area.x] && colors[area.x][area.z]) {
             colors[area.x][area.z] = colors[area.x][area.z] === "white" || colors[area.x][area.z] === "#FFFF00"
-              ? colors[area.x][area.z]
-              : this.displayControl
-                ? empire.color
-                : "transparent";
+            ? colors[area.x][area.z]
+            : this.displayControl
+            ? empire.color
+            : "transparent";
           }
         }
       }
@@ -67,11 +66,11 @@ var app = new Vue({
     nation: function() {
       var res = this.control.find(nation => nation.id === this.navNation);
       var soutiens = [];
-      for (soutien of this.soutiens) {
-        var rank = soutien.soutiens.findIndex(elem => elem === res.player);
+      for (candidate of this.control) {
+        var rank = candidate.soutiens.findIndex(elem => elem === res.id);
         if (rank >= 0) {
           soutiens.push({
-            name: soutien.player,
+            name: candidate.player,
             rank: rank + 1
           });
         }
@@ -79,24 +78,26 @@ var app = new Vue({
       return {
         ...res,
         description: res && res.desc && this.descriptions
-          ? this.descriptions.find(function(desc) {
-            return desc.area === res.desc
-          }).text
-          : "",
+        ? this.descriptions.find(function(desc) {
+          return desc.area === res.desc
+        }).text
+        : "",
         soutiens: soutiens
       };
     },
     border: function() {
       return this.grid
-        ? "1px"
-        : "0px";
+      ? "1px"
+      : "0px";
     },
     nations: function() {
       return this.control.map(nation => {
         var cpt = 0;
-        for (soutien of this.soutiens) {
-          if (soutien.soutiens.find(elem => elem === nation.player)) {
-            cpt += 1;
+        for (candidate of this.control) {
+          for (soutien of candidate.soutiens) {
+            if (soutien === nation.id) {
+              cpt+=1
+            }
           }
         }
         return {
@@ -109,11 +110,71 @@ var app = new Vue({
       return ("```" + JSON.stringify(this.mapEdition.map(area => {
         return {x: area.x, z: area.z};
       })) + "```");
+    },
+    settingsCalc: function() {
+      var ret = {
+        nation: this.settings.soutiens.length<=0?"Aucune":this.settings.soutiens[0].nation
+      }
+      return ret
     }
   },
   methods: {
+    settingsSave: function() {
+      fetch("request/player", {
+        "method": "POST",
+        "headers": {"Content-Type": "application/json"},
+        "body": JSON.stringify({
+          name: this.settings.name,
+          soutiens: this.settings.soutiens.map(soutien => soutien.id),
+          color: this.settings.color,
+          desc: this.settings.desc,
+          image: this.settings.image,
+          nationName: this.settings.nationName
+        })
+      }).then(function(response) {
+        response.json().then(function(json) {
+          console.log(json);
+          this.settings.message = "ℹ La requête a été enregistré, recopier le numéro de commande dans #territoires pour valider : " + json.id
+        }.bind(this));
+      }.bind(this)).catch(err => {
+        console.log(err);
+      });
+    },
+    settingsPlayerChange: function () {
+      var player = this.control.find(elem => elem.id === this.settings.selectedPlayer)
+      this.settings.name = player?player.player:""
+      this.settings.soutiens = player?player.soutiens.map(soutien => {
+        var soutienP = this.control.find(elem => "" + elem.id === soutien)
+        return {id: soutien, name: soutienP?soutienP.player:"unknown", nation: soutienP?soutienP.name:"Aucune"}
+      }):[]
+      this.settings.nationName = !player?"":player.name
+      this.settings.color = !player?"#FF00FF":player.color
+      this.settings.toAddSoutien = ""
+      this.settings.desc = !player||player.desc===undefined?"":player.desc
+      this.settings.image = !player||!player.image?"https://s1.qwant.com/thumbr/0x0/2/f/07b6960b07cf13295400fe43e0c2822a5effc4b4591c082a964b8505ed79e9/33505.jpg?u=http%3A%2F%2Femblemsbf.com%2Fimg%2F33505.jpg&q=0&b=1&p=0&a=1":player.image
+    },
+    settingsSoutienAdd: function(event) {
+      event.preventDefault()
+      var soutienP = this.control.find(elem => "" + elem.id === this.settings.toAddSoutien)
+      this.settings.soutiens.push({id: this.settings.toAddSoutien, name: soutienP?soutienP.player:"", nation: soutienP?soutienP.name:"Aucune"})
+    },
+    settingsEditSoutien: function(event, option, id) {
+      event.preventDefault()
+      switch (option) {
+        case "up":
+        array_move(this.settings.soutiens, id, id - 1)
+        break;
+        case "del":
+        this.settings.soutiens.splice(id, 1)
+        break;
+        default:
+      }
+    },
     setRoute: function(route) {
       this.route = route;
+      if (route === "Options") {
+        this.settings.message = ""
+      }
     },
     setNavNation: function(nation) {
       this.route = "Nation";
@@ -142,20 +203,6 @@ var app = new Vue({
     fetch("data").then(function(response) {
       response.json().then(function(json) {
         this.control = json;
-        for (nation of this.control) {
-          if (nation.desc !== undefined) {
-            fetch("descriptions/" + nation.desc + ".md").then(function(res) {
-              res.text().then(function(text) {
-                this.vue.descriptions.push({area: this.nation.desc, text: text})
-              }.bind(this))
-            }.bind({nation: nation, vue: this}))
-          }
-        }
-      }.bind(this));
-    }.bind(this));
-    fetch("soutiens.json").then(function(response) {
-      response.json().then(function(json) {
-        this.soutiens = json;
       }.bind(this));
     }.bind(this));
   }
