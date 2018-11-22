3db5732ad1a1ed7bd666f37c7336575523177b9c
Erz <lukas.parant@epitech.eu>
2018-11-23 00:44:10 +0100

    invading territorries + 1 request per 24h + better UI

--- bot/bot.js
+++ bot/bot.js
@@ -1,65 +1,190 @@
-const fetch = require("node-fetch")
-const data = require("../data")
+const fetch = require("node-fetch");
+const data = require("../data");
+const moment = require("moment");
+
+const checkUnlimittedPower = async function(idRequesting, idRequested) {
+  var nations = await data.nations.getNations();
+  var soutiensRequesting = 0;
+  var soutiensRequested = 0;
+  for (nation of nations) {
+    var rankRequested = nation.soutiens.findIndex(id => id === idRequested);
+    var rankRequesting = nation.soutiens.findIndex(id => id === idRequesting);
+    if (
+      (rankRequesting >= 0 && rankRequested < 0) ||
+      (rankRequested > rankRequesting && rankRequesting >= 0)
+    ) {
+      soutiensRequesting = soutiensRequesting + 1;
+    } else if (
+      (rankRequested >= 0 && rankRequesting < 0) ||
+      (rankRequesting > rankRequested && rankRequested >= 0)
+    ) {
+      soutiensRequested = soutiensRequested + 1;
+    }
+  }
+  return soutiensRequested < soutiensRequesting;
+};
 
 const processTerritory = async function(author, areas) {
-  var nation = await data.nations.getNation(author.id)
-  var flat = await data.nations.getMap()
+  var nation = await data.nations.getNation(author.id);
+  var flat = await data.nations.getMap();
   if (nation === null || flat === null) {
-    return {success: false, what: "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"}
+    return {
+      success: false,
+      what:
+        "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"
+    };
   }
+  if (nation[0].lastClaim !== undefined && moment(nation[0].lastClaim).add(24, "hours").isAfter(moment())) {
+    return { success: false, what: "c'est une seule déclaration chaque 24h." };
+  }
+  var alsoUpdate = {};
   for (area of areas) {
-    if (flat.map[area.x][area.z].ids.length > 0 && !flat.map[area.x][area.z].ids.find(id => id === author.id)) {
-      return {success: false, what: "un emplacement est **occupé**."}
+    if (
+      flat.map[area.x][area.z].ids.length > 0 &&
+      !flat.map[area.x][area.z].ids.find(id => id === author.id) &&
+      !(await checkUnlimittedPower(author.id, flat.map[area.x][area.z].ids[0]))
+    ) {
+      return {
+        success: false,
+        what: "un emplacement est **occupé** par quelqu'un de plus puissant."
+      };
+    }
+    if (
+      flat.map[area.x][area.z].ids.length > 0 &&
+      !flat.map[area.x][area.z].ids.find(id => id === author.id)
+    ) {
+      alsoUpdate[flat.map[area.x][area.z].ids[0]] = true;
     }
     if (flat.map[area.x][area.z].control === false) {
-      return {success: false, what: "un bout du territoire déclaré est à la flotte, comme ta requête 🎅 "}
+      return {
+        success: false,
+        what:
+          "un bout du territoire déclaré est à la flotte, comme ta requête 🎅 "
+      };
     }
-    flat.map[area.x][area.z].ids = [author.id]
+    flat.map[area.x][area.z].ids = [author.id];
   }
   for (area of areas) {
     if (
       flat.map[area.x - 1][area.z].ids[0] !== author.id &&
       flat.map[area.x + 1][area.z].ids[0] !== author.id &&
       flat.map[area.x][area.z - 1].ids[0] !== author.id &&
-      flat.map[area.x][area.z + 1].ids[0] !== author.id
+      flat.map[area.x][area.z + 1].ids[0] !== author.id &&
+      areas.length > 1
     ) {
-      return {success: false, what: "le territoire déclaré n'est pas continu. Il est juste con."}
+      return {
+        success: false,
+        what: "le territoire déclaré n'est pas continu. Il est juste con."
+      };
     }
   }
   if (nation.length === 0) {
-    if (await data.nations.createNation({id: author.id, name: "Une nation nouvelle", color: "#333333", player: author.username, areas: areas, soutiens: []}) === null) {
-      return {success: false, what: "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"}
+    if (
+      (await data.nations.createNation({
+        id: author.id,
+        name: "Une nation nouvelle",
+        color: "#333333",
+        player: author.username,
+        areas: areas,
+        soutiens: []
+      })) === null
+    ) {
+      return {
+        success: false,
+        what:
+          "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"
+      };
     }
-    return {success: true}
+    return { success: true };
   }
-  if (await data.nations.updateNation(author.id, {areas: areas}) === null) {
-    return {success: false, what: "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"}
+  for (update of Object.keys(alsoUpdate)) {
+    if (
+      (await data.nations.updateNation(update, {
+        areas: []
+      })) === null
+    ) {
+      return {
+        success: false,
+        what:
+          "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"
+      };
+    }
   }
-  return {success: true}
-}
+  if (
+    (await data.nations.updateNation(author.id, {
+      areas: areas,
+      lastClaim: moment()
+    })) === null
+  ) {
+    return {
+      success: false,
+      what:
+        "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"
+    };
+  }
+  if (Object.keys(alsoUpdate).length > 0) {
+    return {
+      success: true,
+      what:
+        ". C'est une invasion redoutable qui a fait sombrer **" +
+        Object.keys(alsoUpdate).length +
+        "** civilisation(s) 💀"
+    };
+  }
+  return { success: true, what: "" };
+};
 
 const processPlayer = async function(author, fields) {
-  var nation = await data.nations.getNation(author.id)
+  var nation = await data.nations.getNation(author.id);
   if (nation === null) {
-    return {success: false, what: "euuuh bug je crois."}
+    return { success: false, what: "euuuh bug je crois." };
   }
-  var newSoutiens = []
+  var newSoutiens = [];
   for (soutien of fields.soutiens) {
     if (!newSoutiens.find(elem => elem === soutien)) {
-      newSoutiens.push(soutien)
+      newSoutiens.push(soutien);
     }
   }
   if (nation.length === 0) {
-    if (await data.nations.createNation({id: author.id, name: fields.nationName, color: fields.color, player: author.username, soutiens: newSoutiens, image: fields.image, leader: fields.name, desc: fields.desc}) === null) {
-      return {success: false, what: "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"}
+    if (
+      (await data.nations.createNation({
+        id: author.id,
+        name: fields.nationName,
+        color: fields.color,
+        player: author.username,
+        soutiens: newSoutiens,
+        image: fields.image,
+        leader: fields.name,
+        desc: fields.desc
+      })) === null
+    ) {
+      return {
+        success: false,
+        what:
+          "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"
+      };
     }
-    return {success: true}
+    return { success: true };
   }
-  if (await data.nations.updateNation(author.id, {name: fields.nationName, color: fields.color, player: author.username, soutiens: fields.soutiens, image: fields.image, leader: fields.name, desc: fields.desc}) === null) {
-    return {success: false, what: "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"}
+  if (
+    (await data.nations.updateNation(author.id, {
+      name: fields.nationName,
+      color: fields.color,
+      player: author.username,
+      soutiens: fields.soutiens,
+      image: fields.image,
+      leader: fields.name,
+      desc: fields.desc
+    })) === null
+  ) {
+    return {
+      success: false,
+      what:
+        "le serveur est ptetre cassé. Au secours @Erzender , vous êtes mon seul espoir"
+    };
   }
-  return {success: true}
-}
+  return { success: true, what: "" };
+};
 
 const processPurge = async function(message) {
   /*
@@ -67,48 +192,55 @@ const processPurge = async function(message) {
     return null
   }
   */
-  var requests = await data.requests.getRequests()
+  var requests = await data.requests.getRequests();
   if (!requests) {
-    return null
+    return null;
   }
-  var cpt = 0
+  var cpt = 0;
   for (request of requests) {
     if (request.id !== 0) {
-      await data.requests.removeRequest(request.id)
-      cpt++
+      await data.requests.removeRequest(request.id);
+      cpt++;
     }
   }
-  await message.reply("la purge youpi: " + cpt + " requêtes assassinnées")
-  return null
-}
+  await message.reply("la purge youpi: " + cpt + " requêtes assassinnées");
+  return null;
+};
 
 exports.processBotMessage = async function(message) {
-  if (!(message.channel.id === JSON.parse(process.env.KELNER_BOT).channel && message.channel.guild.id === JSON.parse(process.env.KELNER_BOT).guild)){
-    return null
+  if (
+    !(
+      message.channel.id === JSON.parse(process.env.KELNER_BOT).channel &&
+      message.channel.guild.id === JSON.parse(process.env.KELNER_BOT).guild
+    )
+  ) {
+    return null;
   }
-  if (message.content === "purge"){
-    return processPurge(message)
+  if (message.content === "purge") {
+    return processPurge(message);
   }
   if (Number(message.content) > 0) {
-    res = await data.requests.getRequest(message.content)
+    res = await data.requests.getRequest(message.content);
     if (res === null) {
-      return null
+      return null;
     }
-    if (res.type==="TERRITORY") {
-      outcome = await processTerritory(message.author, res.payload)
-      await data.requests.removeRequest(message.content)
+    if (res.type === "TERRITORY") {
+      outcome = await processTerritory(message.author, res.payload);
+      await data.requests.removeRequest(message.content);
       if (outcome.success) {
-        return message.reply("le territoire a été **enregistré**")
+        return message.reply(
+          "le territoire a été **enregistré**" + outcome.what
+        );
       }
-      return message.reply("nique ta race Frodon : " + outcome.what)
+      return message.reply("nique ta race Frodon : " + outcome.what);
     }
-    if (res.type==="PLAYER") {
-      outcome = await processPlayer(message.author, res.payload)
-      await data.requests.removeRequest(message.content)
+    if (res.type === "PLAYER") {
+      outcome = await processPlayer(message.author, res.payload);
+      await data.requests.removeRequest(message.content);
       if (outcome.success) {
-        return message.reply("le profile a été **mis à jour**")
+        return message.reply("le profile a été **mis à jour**");
       }
-      return message.reply("nique ta race Frodon : " + outcome.what)
+      return message.reply("nique ta race Frodon : " + outcome.what);
     }
   }
-}
+};
