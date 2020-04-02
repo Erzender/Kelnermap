9598a13d65214a4766ccb99fe4c201a0945eac55
Erzender <lukas.parant@epitech.eu>
2020-04-03 01:01:02 +0200

    better player edifices list

--- src/pres/player.js
+++ src/pres/player.js
@@ -62,7 +62,9 @@ exports.get = async function(req, res) {
           }))
       }))
       .filter(elem => elem.edifices.length > 0)
+      .sort((a, b) => a.edifices.length < b.edifices.length)
   };
+  console.log(info.edifices);
   res.render("index", {
     route: "player",
     embedTitle: info.name,
